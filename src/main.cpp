71d5fa48b318e10d09fe5cf0fa55e3bdcc42114c
fruityloops1 <fruityloops@littun.co>
2025-08-30 23:50:04 +0200

    POUSHE!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "hk/diag/diag.h"
+#include "hk/os/Thread.h"
 #include "hk/services/sm.h"
 #include "hk/services/socket/address.h"
 #include "hk/services/socket/config.h"
@@ -7,21 +8,19 @@
 #include "hk/svc/api.h"
 #include "hk/svc/types.h"
 #include "hk/types.h"
+#include "hk/util/Queue.h"
+#include "hk/util/Storage.h"
 #include "hk/util/Stream.h"
 #include "hk/util/Tuple.h"
+#include "hk/util/Vec.h"
 #include <cstdio>
 
 constexpr hk::socket::ServiceConfig cConfig;
 alignas(hk::cPageSize) u8 buffer[cConfig.calcTransferMemorySize() + 0x20000];
 
-extern "C" void hkMain() {
-    hk::sm::ServiceManager::initialize()->registerClient();
+using LogQueue = hk::util::Queue<hk::Tuple<std::span<const u8>, bool>>;
 
-    hk::Handle port;
-    // make sure to remove if it was left open
-    hk::svc::ManageNamedPort(&port, "hklog", 0);
-    HK_ABORT_UNLESS_R(hk::svc::ManageNamedPort(&port, "hklog", 1));
-
+static void socketThread(LogQueue* queue) {
     auto* socket = hk::socket::Socket::initialize(cConfig, buffer);
 
     auto [err, errno] = socket->socket(hk::socket::AddressFamily::Ipv4, hk::socket::Type::Stream, hk::socket::Protocol(0));
@@ -40,22 +39,46 @@ extern "C" void hkMain() {
     tie(clientFd, errno, client) = socket->accept(fd);
     HK_ABORT_UNLESS(clientFd >= 0, "clientFd: %d, errno: %d", clientFd, errno);
 
-    auto logen = [&](const char* message) {
+    const auto log = [&](const char* message) {
         tie(err, errno) = socket->send(clientFd, std::span<const u8>(cast<const u8*>(message), strlen(message)), 0);
     };
 
-    logen("connected!\n");
+    log("hi\n");
+
+    while (true) {
+        auto [msg, shouldNewline] = queue->take();
+
+        tie(err, errno) = socket->send(clientFd, msg, 0);
+        if (shouldNewline)
+            log("\n");
+
+        delete[] msg.data();
+    }
+}
+
+extern "C" void hkMain() {
+    hk::sm::ServiceManager::initialize()->registerClient();
+
+    hk::Handle port;
+    // make sure to remove if it was left open
+    hk::svc::ManageNamedPort(&port, "hklog", 0);
+    HK_ABORT_UNLESS_R(hk::svc::ManageNamedPort(&port, "hklog", 1));
+
+    LogQueue queue;
+    hk::os::Thread thread(socketThread, &queue);
+    thread.setName("SocketThread");
+    thread.start();
 
     std::array<u8, 28> data = {};
     struct Handles {
         hk::Handle port;
         hk::Handle client = 0;
     } handles = { .port = port };
-    bool sendBack = false;
+    bool reply = false;
     while (true) {
         u32 handleIndex = 0;
-        auto result = hk::svc::ReplyAndReceive(&handleIndex, &handles.port, handles.client ? 2 : 1, sendBack ? handles.client : 0, -1);
-        sendBack = false;
+        auto result = hk::svc::ReplyAndReceive(&handleIndex, &handles.port, handles.client ? 2 : 1, reply ? handles.client : 0, -1);
+        reply = false;
         switch (result.getValue()) {
         case 0: {
             if (handleIndex == 0) {
@@ -67,30 +90,22 @@ extern "C" void hkMain() {
             auto buffer = stream.read<hk::sf::hipc::Buffer>();
             if (header.sendBufferCount != 1) {
                 hk::svc::CloseHandle(handles.client);
-                logen("invalid handle :(");
+                HK_ABORT("invalid handle :(", 0);
                 continue;
             }
 
-            while (true) {
-                if (clientFd < 0) {
-                    tie(clientFd, errno, client) = socket->accept(fd);
-                    HK_ABORT_UNLESS(clientFd >= 0, "clientFd: %d, errno: %d", clientFd, errno);
-                    logen("connected!\n");
-                }
-                tie(err, errno) = socket->send(clientFd, std::span<const u8>(cast<const u8*>(buffer.address()), buffer.size()), 0);
-                if (header.tag == 0)
-                    logen("\n");
-                if (err < 0) {
-                    socket->close(clientFd);
-                    hk::svc::Break(hk::svc::BreakReason_User, nullptr, errno);
-                    clientFd = -1;
-                    continue;
-                }
-                break;
+            u8* msg = new u8[buffer.size()];
+            if (msg == nullptr)
+                while (!queue.empty())
+                    delete[] queue.take().a.data();
+            else {
+                memcpy(msg, cast<const void*>(buffer.address()), buffer.size());
+                queue.add({ { msg, buffer.size() }, header.tag == 0 });
             }
+
             stream.seek(0);
             stream.write(hk::sf::hipc::Header { .dataWords = 4 });
-            sendBack = true;
+            reply = true;
             continue;
         }
         case 0xf601: {
@@ -102,11 +117,8 @@ extern "C" void hkMain() {
             continue;
         }
         default: {
-            char message[512];
-            std::sprintf(message, "new and unique result %x\n", result.getValue());
-            logen(message);
+            HK_ABORT("new and unique result %x", result.getValue());
         }
         }
     }
-    HK_ABORT("bye %d %d", err, errno);
 }
